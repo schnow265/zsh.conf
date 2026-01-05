6737dad61410781fb076c4577807caf60ad46fa5
schnow265 <thesnowbox@icloud.com>
2026-01-05 20:38:27 +0100

    remove full path

--- cfg/06_path.zsh
+++ cfg/06_path.zsh
@@ -1,10 +1,10 @@
 # PATH expansion
 
 . "$HOME/.local/bin/env"
-export PATH="$HOME/.aspire/bin:$HOME/.local/share/gem/ruby/3.4.0/bin:$PATH:/home/schnow265/.dotnet/tools"
+export PATH="$HOME/.aspire/bin:$HOME/.local/share/gem/ruby/3.4.0/bin:$PATH:$HOME/.dotnet/tools"
 
 # pnpm
-export PNPM_HOME="/home/schnow265/.local/share/pnpm"
+export PNPM_HOME="$HOME/.local/share/pnpm"
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
