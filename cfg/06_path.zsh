1607ec684b6318db6081f1e4913d5c61461e9316
schnow265 <thesnowbox@icloud.com>
2026-01-05 08:10:27 +0100

    added scripts to repo

--- cfg/06_path.zsh
+++ cfg/06_path.zsh
@@ -1,6 +1,9 @@
 # PATH expansion
+
+ZSH_CFG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}/zsh"
+
 . "$HOME/.local/bin/env"
-export PATH="$HOME/.aspire/bin:$HOME/.local/share/gem/ruby/3.4.0/bin:$PATH:/home/schnow265/.dotnet/tools"
+export PATH="$ZSH_CFG_HOME/bin:$HOME/.aspire/bin:$HOME/.local/share/gem/ruby/3.4.0/bin:$PATH:/home/schnow265/.dotnet/tools"
 
 # pnpm
 export PNPM_HOME="/home/schnow265/.local/share/pnpm"
