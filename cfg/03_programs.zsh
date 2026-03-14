01b22b5d046e63c42df0742f8706d1ef8c2f26b7
schnow265 <schnow265@schnow265.dev>
2026-03-14 16:21:24 +0100

    remove programs

--- cfg/03_programs.zsh
+++ cfg/03_programs.zsh
@@ -1,18 +1,5 @@
 zinit as"program" from"gh-r" for kitware/cmake
-
 zinit as"program" from"gh-r" for ccache/ccache
-
-zinit as"program" make'install' atclone'make CMAKE_INSTALL_PREFIX=$ZPFX CMAKE_BUILD_TYPE=RelWithDebInfo' atpull'%atclone' pick"$ZPFX/bin/nvim" for \
-    neovim/neovim
-
-zinit as"program" cmake'--install build' atclone'cmake -B build -G Ninja && cmake --build build' atpull'%atclone' pick"$ZPFX/bin/btop" for \
-    aristocratos/btop
-
-zinit as"program" make'!' atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' pick"direnv" src"zhook.zsh" for \
-    direnv/direnv
-
-zinit as"program" make'install' atclone'./configure --prefix=$ZPFX' atpull'%atclone' pick"$ZPFX/bin/fizsh" for \
-    zsh-users/fizsh
-
 zinit as"program" from"gh-r" for @astral-sh/uv
-zinit as"program" from"gh-r" for neovide/neovide
+
+zinit as"program" make'!' atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone' pick"direnv" src"zhook.zsh" for direnv/direnv
